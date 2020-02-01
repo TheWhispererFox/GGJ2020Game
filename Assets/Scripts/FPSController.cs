1631ad395a69d5f0e6f0c8986ddfe0968e673c45
TheWhisperer <houstonplanes@gmail.com>
2020-02-01 21:11:58 +0300

    Added FPS Controller, Lasso (WIP)

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
+{
+	public float gravity = 9.81f;
+	public float speed = 5f;
+	public float sprintSpeedModifier = 1.5f;
+	public float mouseSensitivity = 25f;
+	public Transform cameraTransform;
+	public float jumpHeight = 10f;
+	public LayerMask groundMask;
+	public Transform groundCheckObject;
+	public float groundDistance = 0.4f;
+	public bool lockCursor = false;
+
+	private CharacterController _character;
+	private float yRotation = 0f;
+	private Vector3 velocity;
+	private bool isGrounded;
+
+	private void Start()
+	{
+		_character = GetComponent<CharacterController>();
+		if (lockCursor)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+	}
+	private void Update()
+	{
+		isGrounded = Physics.CheckSphere(groundCheckObject.position, groundDistance, groundMask);
+
+		float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+		float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+
+		float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			x *= sprintSpeedModifier;
+			z *= sprintSpeedModifier;
+		}
+
+		Vector3 move = transform.right * x + transform.forward * z;
+
+		yRotation -= mouseY;
+		yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+		cameraTransform.transform.localRotation = Quaternion.Euler(yRotation, 0, 0);
+		transform.Rotate(Vector3.up * mouseX);
+		_character.Move(move * Time.deltaTime * speed);
+
+		// ---------------------------------------------------------------------------
+
+		if (isGrounded && velocity.y < 0)
+		{
+			velocity.y = -2f;
+		}
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * -gravity);
+		}
+		velocity.y -= gravity * Time.deltaTime;
+		_character.Move(velocity * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/LassoHandler.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LassoHandler : MonoBehaviour
+{
+	public float throwForce = 5f;
+	public Transform lasso;
+	public Vector3 lassoThrowingPosition = new Vector3(0, 1f, 2f);
+	public Transform owner;
+	public Slider powerMeter;
+	public float maxHoldTime = 3f;
+
+	private bool isThrown;
+	private Vector3 originalPos;
+	private Rigidbody rb;
+	private float holdTime;
+	private float debounce;
+
+	private void Start()
+	{
+		originalPos = lasso.localPosition;
+		rb = lasso.GetComponent<Rigidbody>() ?? lasso.gameObject.AddComponent<Rigidbody>();
+		powerMeter.maxValue = maxHoldTime;
+	}
+
+	private void Update()
+	{
+		if (debounce <= 0)
+		{
+			debounce = 2f;
+		}
+		debounce -= Time.deltaTime;
+		if (Input.GetButtonDown("Fire1"))
+		{
+			if (isThrown)
+			{
+				RestoreLasso();
+				return;
+			}
+		}
+		if (holdTime >= maxHoldTime)
+		{
+			ThrowLasso();
+			return;
+		}
+		if (Input.GetButton("Fire1"))
+		{
+			if (isThrown && debounce > 0) return;
+			holdTime += Time.deltaTime;
+			powerMeter.value = holdTime;
+		}
+		if (Input.GetButtonUp("Fire1"))
+		{
+			if (isThrown) return;
+			ThrowLasso();
+		}
+	}
+
+	private void ThrowLasso()
+	{
+		lasso.localPosition = lassoThrowingPosition;
+		rb.useGravity = true;
+		rb.isKinematic = false;
+		lasso.SetParent(null);
+		rb.AddForce(throwForce * holdTime * Camera.main.transform.forward, ForceMode.Impulse);
+		isThrown = !isThrown;
+		holdTime = 0;
+		powerMeter.value = 0;
+	}
+
+	private void RestoreLasso()
+	{
+		lasso.SetParent(owner);
+		lasso.localPosition = originalPos;
+		rb.useGravity = false;
+		rb.isKinematic = true;
+		isThrown = !isThrown;
+	}
+}

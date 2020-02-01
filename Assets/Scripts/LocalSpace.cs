29a40b41cf86edc52b4ce988586c0219c3d51c79
IlyaGavshin <gafhin03@gmail.com>
2020-02-01 21:15:53 +0300

    Added Shakals

---
+++ Assets/Scripts/LocalSpace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalSpace : MonoBehaviour
+{
+    public static LocalSpace globalPos { get; private set; }
+
+    [HideInInspector]
+    public Vector3 pos;
+
+    private void Awake()
+    {
+        globalPos = this;
+    }
+
+    public Vector3 RandomSpawnPos()
+    {
+        pos = new Vector3(Random.Range(-Spawner.radius, Spawner.radius), 0, Random.Range(4, -Spawner.radius));
+        return transform.TransformPoint(pos);
+    }
+    public Vector3 RandomPointInRadius()
+    {
+        pos = new Vector3(Random.Range(-Spawner.radius, Spawner.radius), 0, Random.Range(Spawner.radius, -Spawner.radius));
+        return transform.TransformPoint(pos);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Set  Dynimacly")]
+    public GameObject prefDog;
+    public List<GameObject> dogs;
+    public int Lengths;
+    public float radiuS = 50f;
+    static public float radius;
+
+    void Awake()
+    {
+        radius = radiuS;
+        dogs = new List<GameObject>();
+        for(int i = 0; i < Lengths; i++)
+        {
+            dogs.Add(Instantiate(prefDog, LocalSpace.globalPos.RandomSpawnPos(), Quaternion.identity));
+
+        }
+    }
+}

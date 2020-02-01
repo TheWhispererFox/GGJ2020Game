03004fe9e3d44fac25b63b9e53dc76083b1ee1db
Amir Niyazov <Amir.Niyazov.2001.AN@gmail.com>
2020-02-01 21:08:06 +0300

    added image opening and dominationg color calculatiing.

---
+++ Assets/Scripts/DominantColor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// BUG: Make autoupdate pic color on image change
+public class DominantColor : MonoBehaviour
+{
+    private readonly Dictionary<string, Color> _colors = new Dictionary<string, Color>
+    {
+        {"Red", Color.red},
+        {"Orange", new Color(255, 165, 0)},
+        {"Yellow", Color.yellow},
+        {"Green", Color.green},
+        {"White", Color.white},
+        {"Grey", Color.grey},
+        {"Black", Color.black},
+        {"Violet", new Color(138, 43, 226)},
+        {"Blue", Color.blue},
+        {"Light blue", new Color(135, 206, 250)}
+    };
+    private Color _color;
+    public void Start()
+    {
+        if (GetComponent<Image>().sprite != null) CalculateDominantColor();
+    }
+
+    private void CalculateDominantColor()
+    {
+        var oftenColor = new ColorThief.ColorThief().GetColor(DuplicateTexture(GetComponent<Image>().sprite.texture))
+            .UnityColor;
+
+        (Color, double) nearestColor = (Color.white, 0);
+
+        foreach (var color in _colors)
+        {
+            var distance = Math.Sqrt(Math.Pow(nearestColor.Item1.r - oftenColor.r, 2) +
+                                     Math.Pow(nearestColor.Item1.g - oftenColor.g, 2) +
+                                     Math.Pow(nearestColor.Item1.b - oftenColor.b, 2));
+            if (nearestColor.Item2 < 255) nearestColor = (color.Value, distance);
+        }
+
+        _color = nearestColor.Item1;
+    }
+
+    private static Texture2D DuplicateTexture(Texture2D source)
+    {
+        var renderTex = RenderTexture.GetTemporary(
+            source.width,
+            source.height,
+            0,
+            RenderTextureFormat.Default,
+            RenderTextureReadWrite.Linear);
+
+        Graphics.Blit(source, renderTex);
+        var previous = RenderTexture.active;
+        RenderTexture.active = renderTex;
+        var readableText = new Texture2D(source.width, source.height);
+        readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+        readableText.Apply();
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(renderTex);
+        return readableText;
+    }
+}
---
+++ Assets/Scripts/ImageOpenScript.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using SFB;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageOpenScript : MonoBehaviour
+{
+    public void OpenAndReplaceTexture()
+    {
+        var canvasRenderer = GetComponent<CanvasRenderer>();
+        var image = canvasRenderer.GetComponent<Image>();
+        var openFile = StandaloneFileBrowser.OpenFilePanel(
+            "Open file",
+            "",
+            new[] {new ExtensionFilter("Images", "jpg", "png")},
+            false);
+
+        
+        image.sprite = Sprite.Create(
+            LoadImage(openFile[0]),
+            new Rect(0, 0, 1280, 720),
+            image.sprite.textureRectOffset);
+    }
+
+    private static Texture2D LoadImage(string filename)
+    {
+        if (!File.Exists(filename)) return null;
+
+        var texture = new Texture2D(1920, 1080);
+        var bytes = File.ReadAllBytes(filename);
+        texture.LoadImage(bytes);
+        return texture;
+    }
+}
